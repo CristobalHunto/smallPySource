5747d7ecd4f4bcf2d1eaba048e1c59af23d79339
CristobalHunto <76532078+CristobalHunto@users.noreply.github.com>
2021-02-10 00:08:45 +0700

    Example of working with big data with Pandas
    We filter the big file (csv) so that the second column is present in the criterion file. 
    Фильтруем большой файл (csv) так, чтобы второй столбец присутствовал в файле критерия.

---
+++ LookBigDB.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+FILES_NAME = {'file_prefix': f'bandwidth.csv',
+              'file_data':   f'sample_big.csv',
+              'file_result': f'out.csv'}
+
+CHUNK_SIZE = 10 # Need to change 10 to 1000000 or 10000000
+
+
+dbprefix = pd.read_csv(FILES_NAME['file_prefix'], header=None, delimiter=' ', names=['b'])
+
+verification_list = dbprefix['b'].tolist()
+i = 0   # count of looking chunks
+mode_write = 'w'
+
+for chunk in pd.read_csv(FILES_NAME['file_data'], chunksize=CHUNK_SIZE,
+                         header=None, names=['a', 'b']):
+
+    print('viewed ', i * CHUNK_SIZE,chunk.l, end='')
+    records_found = chunk[chunk['b'].isin(verification_list)]
+    if records_found.empty:
+        print(' no matches')
+
+    else:
+        records_found.to_csv(FILES_NAME['file_result'],
+                             mode=mode_write, header=False, index=False)
+        print(' records ', len(records_found))
+        mode_write = 'a'
+        
+    i += 1
+
+print('end')
